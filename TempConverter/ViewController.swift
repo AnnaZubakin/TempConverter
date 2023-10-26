afd36741e4a0f8cf9bf507d8b0ad739b45a05496
AnnaZubakin <146950990+AnnaZubakin@users.noreply.github.com>
2023-10-26 16:40:14 +0300

    TempCalculatorOK

--- TempConverter/ViewController.swift
+++ TempConverter/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var celsiusLabel: UILabel!
     @IBOutlet weak var convertedTempLabel: UILabel!
     @IBOutlet weak var tempSegmentControl: UISegmentedControl!
-    @IBOutlet weak var tempSlider: UISlider! {
+    @IBOutlet weak var tempSlider: UISlider!
+    {
         didSet {
             tempSlider.value = 0
             tempSlider.minimumValue = 0
@@ -21,24 +22,40 @@ class ViewController: UIViewController {
     }
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        convertedTempLabel.text = "32 F"
-    }
-
-
+          super.viewDidLoad()
+          updateTempLabelFromSlider(value: tempSlider.value)
+      }
+      
+ 
     @IBAction func sliderValueChanged(_ sender: Any) {
         updateTempLabelFromSlider(value: tempSlider.value)
     }
-    func updateTempLabelFromSlider(value: Float) {
-        print("tempSlider:::", Int(value))
-        #warning("check which segment controller is on and make a temp calculation based F/K")
-    }
     
-    func convertTempFrom(celsius: Int) -> (fahrenheit: Double, kelvin: Double) {
-        return (0,0)
-    }
-  
-    #warning("pass result to InfoVC without segue")
-    
-}
+      func updateTempLabelFromSlider(value: Float) {
+          let celsiusTemperature = Int(value)
+          let (fahrenheitTemperature, kelvinTemperature) = convertTempFrom(celsius: celsiusTemperature)
+          
+          let selectedSegmentIndex = tempSegmentControl.selectedSegmentIndex
+          var convertedTemperature: Double
+          
+          switch selectedSegmentIndex {
+          case 0: // Фаренгейты
+              convertedTemperature = fahrenheitTemperature
+              convertedTempLabel.text = "\(convertedTemperature) F"
+          case 1: // Келвины
+              convertedTemperature = kelvinTemperature
+              convertedTempLabel.text = "\(convertedTemperature) K"
+          default:
+              break
+          }
+          
+          celsiusLabel.text = "\(celsiusTemperature) °C"
+      }
+      
+      func convertTempFrom(celsius: Int) -> (fahrenheit: Double, kelvin: Double) {
+          let fahrenheit = Double(celsius) * 9/5 + 32
+          let kelvin = Double(celsius) + 273.15
+          return (fahrenheit, kelvin)
+      }
+  }
 
